780ed2371df39582ac1caa6a24d9397c1e2132a9
namnguyen215 <namnguyenphuong215@gmail.com>
2022-06-09 22:28:13 +0700

    nam

--- src/main/java/org/example/GetResult.java
+++ src/main/java/org/example/GetResult.java
@@ -5,7 +5,7 @@ import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
 import static com.swoop.alchemy.spark.expressions.hll.functions.hll_cardinality;
 import static org.apache.spark.sql.functions.col;
-
+import static org.apache.spark.sql.functions.lit;
 public class GetResult {
 
     private static  String hdfsPath = "hdfs://internship-hadoop105185:8120/mydata/";
@@ -19,7 +19,7 @@ public class GetResult {
                 .builder()
                 .master("yarn")
                 .getOrCreate();
-        spark.sparkContext().setLogLevel("ERROR");
+//        spark.sparkContext().setLogLevel("ERROR");
         Dataset<Row> df = spark.read()
                 .format("jdbc")
                 .option("driver", "com.mysql.cj.jdbc.Driver")
@@ -28,7 +28,7 @@ public class GetResult {
                 .option("user", "namnp")
                 .option("password", "12345678")
                 .load();
-        Dataset<Row> res = df.filter(col("Day").$eq$eq$eq(date));
+        Dataset<Row> res = df.filter(col("Day").equalTo(date));
         res.select(col("bannerId"),
                         hll_cardinality("guid_hll").as("Number of guids"))
                 .show(false);
